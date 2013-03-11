3e3bd845f2dc1c269dfc870b7968d34dab3dbc6a
GrNik <gr_nik@mail.ru>
2013-03-11 18:08:35 +0400

    Start1

--- Educations/Main.Designer.cs
+++ Educations/Main.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.ClientSize = new System.Drawing.Size(996, 789);
             this.Name = "Main";
             this.Text = "Образование";
             this.ResumeLayout(false);
