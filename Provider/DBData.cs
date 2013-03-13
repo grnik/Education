0a1269ed0744e5c2eb81f71ee497fd5d7474876e
GrNik <gr_nik@mail.ru>
2013-03-13 18:01:34 +0400

    Create provider

---
+++ Provider/DBData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Provider
+{
+    internal class DBData
+    {
+        private const string ConnectionString = @"Data Source=C0009;Initial Catalog=Education;Integrated Security=True";
+
+        public SqlConnection connection { get; private set; }
+        
+        public DBData()
+        {
+            connection = new SqlConnection(ConnectionString);
+        }
+    }
+}
---
+++ Provider/Education/Lesson.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Provider.Education
+{
+    using System.Data;
+
+    public class Lesson
+    {
+        DBData db = new DBData();
+        
+        public const string TableName = "Lesson";
+
+        /// <summary>
+        /// Вернуть список всех уроков.
+        /// </summary>
+        public DataTable All()
+        {
+            try
+            {
+                using (SqlCommand comm = new SqlCommand())
+                {
+                    comm.Connection = db.connection;
+                    comm.CommandText = "select * from Lesson";
+
+                }
+            }
+            catch (Exception)
+            {
+                return new DataTable(TableName);
+            }
+        }
+    }
+}
